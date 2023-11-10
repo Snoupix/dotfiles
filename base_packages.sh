994139b934df0273d042a349d8ccfee468df6929
Snoupix <samufpsdu59@hotmail.fr>
2023-11-10 18:14:14 +0100

    Added post commands after base packages install

--- base_packages.sh
+++ base_packages.sh
@@ -53,12 +53,20 @@ for V in "${PM_FLAGS[@]}"; do
     fi
 done
 
+# Post commands
+
 rustup default stable
 rustup component add cargo rustfmt clippy rust-src rust-analyzer rustc rust-docs
 rustup update
 
 cargo install --locked zellij
 cargo install ripgrep
+
+nvim --headless '+Lazy! restore' +qa
+
+sh -c $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 echo "Done!"
 exit 0
