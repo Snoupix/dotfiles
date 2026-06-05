2d91f7bf2bcbec1983b82bea105e92fcd1d399fc
Snoupix <snoupix.pro@proton.me>
2026-06-05 21:42:38 +0200

    fix(scripts): sysmon launch fix(wallpaper): init awww daemon on-launch

--- _hypr/hypr/scripts/sysmonlaunch.sh
+++ _hypr/hypr/scripts/sysmonlaunch.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 # Define the list of commands to check in order of preference
-commands_to_check=("htop" "btop" "top")
+commands_to_check=("btop" "htop" "top")
 
 # Determine the terminal emulator to use
-term=$(cat $HOME/.config/hypr/keybindings.conf | grep ^'$term' | cut -d '=' -f2)
+term=$(cat $HOME/.config/hypr/keybindings.lua | grep 'term' | head -1 | cut -d '=' -f2 | sed "s/[\s']*//g")
 
 # Try to execute the first available command in the specified terminal emulator
 for command in "${commands_to_check[@]}"; do
